df0644dbfd66f9655818e615c0307f78189bc345
Gordon Choi <thefirate@gmail.com>
2024-05-07 21:22:38 +0900

    기반 프로젝트 셋업

---
+++ SomethingUsefulSwiftUI/SomethingUsefulSwiftUI/SomethingUsefulSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomethingUsefulSwiftUIApp.swift
+//  SomethingUsefulSwiftUI
+//
+//  Created by Gordon Choi on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SomethingUsefulSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SomethingUsefulUIKit/SomethingUsefulUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomethingUsefulUIKit
+//
+//  Created by Gordon Choi on 5/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
