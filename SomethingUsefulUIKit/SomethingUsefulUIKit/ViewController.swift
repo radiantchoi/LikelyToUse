79530a4bf4442f4d7dca5fc904beaee77a7cac16
Gordon Choi <thefirate@gmail.com>
2024-06-25 21:53:04 +0900

    UIKit Lottie 적용

--- SomethingUsefulUIKit/SomethingUsefulUIKit/ViewController.swift
+++ SomethingUsefulUIKit/SomethingUsefulUIKit/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import Lottie
+
 final class ViewController: BaseViewController {
     private lazy var helloLabel: UILabel = {
         let label = UILabel()
@@ -15,6 +17,14 @@ final class ViewController: BaseViewController {
         return label
     }()
     
+    private lazy var lottieAnimatedView: LottieAnimationView = {
+        $0.loopMode = .loop
+        $0.animationSpeed = 2
+        $0.backgroundColor = .clear
+        
+        return $0
+    }(LottieAnimationView(name: "Wave"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -22,13 +32,19 @@ final class ViewController: BaseViewController {
     // 이런 식으로 쓸 때 super를 해 주어야 할까?
     override func setupSubviews() {
         view.addSubview(helloLabel)
+        view.addSubview(lottieAnimatedView)
+        
+        lottieAnimatedView.play()
     }
     
     override func setupConstraints() {
         helloLabel.translatesAutoresizingMaskIntoConstraints = false
+        lottieAnimatedView.translatesAutoresizingMaskIntoConstraints = false
         
         helloLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         helloLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        lottieAnimatedView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        lottieAnimatedView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }
 
